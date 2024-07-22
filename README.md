9dd9f1b651fd1ba02d8a8933b332f45cbdaa698e
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-07-22 20:14:07 -0300

    style: fix style

--- README.md
+++ README.md
@@ -11,8 +11,13 @@ ou
 ****** Ligador
 
 *** O ligador deve chamar LIGADOR. 
+
 *** Deve ser usado “./ligador prog1.obj prog2.obj”. 
+
 *** Deve dar como saida um arquivo chamado “prog1.e”. 
+
 *** A entrada do ligador deve ser os arquivos obj gerados pelo seu montador em caso de ter achado diretivas BEGIN e END. 
+
 *** O arquivo de saida “.e” deve ser um arquivo de texto de uma unica linha igual ao OBJ q nao precisa der ligado. 
+
 *** Deve ser capa de rodar no simulador. O ligador funciona no máximo com 2 arquivos unicamente
--- main.c
+++ main.c
@@ -6,39 +6,38 @@ int main(int argc, char *argv[])
 {
     if(argv[1]!=NULL && argv[2]==NULL)
     {
-        // preProcessor(argv[2]);
         FILE *pFileRead, *pFileWrite;
-        
 
-        char lineContent[512];
-        char *temp;
+        char *temp, lineContent[512];
 
         temp = argv[1];
-
-	    pFileRead = fopen(argv[1], "r");
-	    if (pFileRead == NULL) {
+        
+        pFileRead = fopen(argv[1], "r");
+        if (pFileRead == NULL) 
+        {
             printf("Erro ao abrir arquivo.\n");
             exit(1);
         }
-
-		pFileWrite = fopen(strcat(strtok(temp, "."),".e"), "w");
-        if (pFileWrite == NULL) {
+        
+        pFileWrite = fopen(strcat(strtok(temp, "."),".e"), "w");
+        if (pFileWrite == NULL) 
+        {
             printf("Erro ao criar arquivo.\n");
             exit(1);
         }
-
-		while (fgets(lineContent, 512, pFileRead) != NULL)
-		{
-			fputs(lineContent, pFileWrite);
-		}
-
+        
+        while (fgets(lineContent, 512, pFileRead) != NULL)
+        {
+            fputs(lineContent, pFileWrite);
+        }
+        
         fclose(pFileRead);
         fclose(pFileWrite);
-
     } 
-    else {
+    else 
+    {
         printf("Quantidade de argumentos nao suportado.");
     }
-
+    
     return 0;
 }
